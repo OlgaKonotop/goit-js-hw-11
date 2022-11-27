5ad08b7e501724bbbb6ca35c0aa16171e0f13ed7
OlgaKonotop <konotop-olga@ukr.net>
2022-11-27 16:11:57 +0200

    start02

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log(555);
 import axios from 'axios';
 
-const axios = require('axios').default;
+//const axios = require('axios').default;
 console.log(axios);
 // axios.get('/users').then(res => {
 //   console.log(res.data);
